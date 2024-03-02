34d05e44bb13977d82e4083b983c4c2229cdcb92
Dhruv <dhruvgopani8@gmail.com>
2024-03-02 17:11:26 +0530

    initial2

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -8,58 +8,58 @@ import { NextResponse } from "next/server";
 const settingsUrl=absoluteUrl("/settings");
 
 export async function GET(){
-    try{
-        const {userId}=auth();
-        const user=await currentUser();
+    // try{
+    //     const {userId}=auth();
+    //     const user=await currentUser();
 
-        if(!userId || !user)return new NextResponse("Unauthorized",{status:401})
+    //     if(!userId || !user)return new NextResponse("Unauthorized",{status:401})
 
-        const userSubscription=await prismadb.userSubscription.findUnique({
-            where:{
-                userId
-            }
-        })
-        if(userSubscription && userSubscription.stripeCostumerId){
-            const stripeSession=await stripe.billingPortal.sessions.create({
-                customer:userSubscription.stripeCostumerId,
-                return_url:settingsUrl,
+    //     const userSubscription=await prismadb.userSubscription.findUnique({
+    //         where:{
+    //             userId
+    //         }
+    //     })
+    //     if(userSubscription && userSubscription.stripeCostumerId){
+    //         const stripeSession=await stripe.billingPortal.sessions.create({
+    //             customer:userSubscription.stripeCostumerId,
+    //             return_url:settingsUrl,
 
-            });
-            return new NextResponse(JSON.stringify({url:stripeSession.url}));
-        }
-        const stripeSession=await stripe.checkout.sessions.create({
-            success_url:settingsUrl,
-            cancel_url:settingsUrl,
-            payment_method_types:["card"],
-            mode:"subscription",
-            billing_address_collection:"required",
-            customer_email:user.emailAddresses[0].emailAddress,
-            line_items:[
-                {
-                    price_data:{
-                        currency:"INR",
-                        product_data:{
-                            name:"Creator Pro",
-                            description:"Unlimited AI Generation",
-                        },
-                        unit_amount:800000,
-                        recurring:{
-                            interval:"month"
-                        }
-                    },
-                    quantity:1
-                }
-            ],
-            metadata:{
-                userId,
-            }
-        })
-        return new NextResponse(JSON.stringify({url:stripeSession.url}))
+    //         });
+    //         return new NextResponse(JSON.stringify({url:stripeSession.url}));
+    //     }
+    //     const stripeSession=await stripe.checkout.sessions.create({
+    //         success_url:settingsUrl,
+    //         cancel_url:settingsUrl,
+    //         payment_method_types:["card"],
+    //         mode:"subscription",
+    //         billing_address_collection:"required",
+    //         customer_email:user.emailAddresses[0].emailAddress,
+    //         line_items:[
+    //             {
+    //                 price_data:{
+    //                     currency:"INR",
+    //                     product_data:{
+    //                         name:"Creator Pro",
+    //                         description:"Unlimited AI Generation",
+    //                     },
+    //                     unit_amount:800000,
+    //                     recurring:{
+    //                         interval:"month"
+    //                     }
+    //                 },
+    //                 quantity:1
+    //             }
+    //         ],
+    //         metadata:{
+    //             userId,
+    //         }
+    //     })
+    //     return new NextResponse(JSON.stringify({url:stripeSession.url}))
 
-    }catch (error){
-        console.log("[STRIPE ERROR]",error);
-        return new NextResponse("Internal error",{status:500});
+    // }catch (error){
+    //     console.log("[STRIPE-ERROR]",error);
+    //     return new NextResponse("Internal error",{status:500});
 
 
-    }
+    // }
 }

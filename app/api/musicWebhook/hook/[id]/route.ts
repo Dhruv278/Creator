f8efbd617473e996c46e4977aafc5c002109b8d4
Dhruv <dhruvgopani8@gmail.com>
2024-03-06 20:53:22 +0530

    update middleware 2

--- app/api/musicWebhook/hook/[id]/route.ts
+++ app/api/musicWebhook/hook/[id]/route.ts
@@ -1,7 +1,6 @@
 
-import { increaseApiLimit } from "@/lib/api-limit";
-import {  getMusicData, setMusicData } from "@/lib/utils";
-import { auth } from "@clerk/nextjs";
+
+import { setMusicData } from "@/lib/utils";
 import { NextResponse } from "next/server"
 
 export async function POST(req:Request,context:any){
@@ -9,21 +8,9 @@ export async function POST(req:Request,context:any){
     const id=params.id;
     console.log("data fetched")
 
-    // const final=await req.json();
+    const final=await req.json();
     
-    // await setMusicData(id,final.output.audio)
+    await setMusicData(id,final.output.audio)
     return new NextResponse(id,{status:200})
     
-}
-export async function GET(req:Request,context:any){
-    const {userId}=auth();
-    if(!userId)return new NextResponse("Unauthorized",{status:401});
-
-    let musicData=await getMusicData(userId);
-    // console.log(musicData);
-    if(musicData===undefined)return new NextResponse(undefined,{status:204})
-    
-    await increaseApiLimit();
-
-    return new NextResponse(JSON.stringify({url:musicData}),{status:200});
 }
